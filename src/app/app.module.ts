22adcba9296d7b35449545610dd6465701c02c51
Lakshmi S <lakshmi.s@gramener.org>
2021-04-28 20:37:32 +0530

    angular intiated and added routes

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BioComponent } from './bio/bio.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    BioComponent,
+    WorkComponent,
+    ContactComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomepageComponent},
+      {path: 'home', component: HomepageComponent},
+      {path: 'bio', component: BioComponent},
+      {path: 'work', component: WorkComponent},
+      {path: 'contact', component: ContactComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
